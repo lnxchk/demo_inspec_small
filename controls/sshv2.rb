172a811f8037b91364a396384d01f9bd2d95dcb4
Mandi Walls <mandi.walls@gmail.com>
2018-06-21 12:36:08 +0100

    fixing sshv2

--- controls/sshv2.rb
+++ controls/sshv2.rb
@@ -1,6 +1,6 @@
 # encoding: utf-8
 #
-describe sshd_config do
+control 'sshd_config-01' do
   impact 1.0
 
   title 'SSH Version 2'
@@ -8,8 +8,9 @@ describe sshd_config do
   desc <<-EOF
     SSH supports two different protocol versions. The original version, SSHv1, was subject to a number of security issues. Please use SSHv2 instead to avoid these.
   EOF
-
-  its('Protocol') { should cmp 2 }
+  describe sshd_config do {
+    its('Protocol') { should cmp 2 }
+  }
 end
 
 
